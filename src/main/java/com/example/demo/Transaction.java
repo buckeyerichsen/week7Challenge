13b7c49f5d521d98d86cbbd7fee7038dc1b89618
buckeyerichsen <buckey.erichsen@gmail.com>
2019-10-18 13:25:47 -0400

    almost

--- src/main/java/com/example/demo/Transaction.java
+++ src/main/java/com/example/demo/Transaction.java
@@ -10,6 +10,8 @@ public class Transaction {
     private long transactionid;
     private String action;
     private String accountnumber;
+    private String deposit;
+    private String withdrawal;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "account_id")
@@ -45,5 +47,21 @@ public class Transaction {
 
     public void setAccount(Account account) {
         this.account = account;
+    }
+
+    public String getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(String deposit) {
+        this.deposit = deposit;
+    }
+
+    public String getWithdrawal() {
+        return withdrawal;
+    }
+
+    public void setWithdrawal(String withdrawal) {
+        this.withdrawal = withdrawal;
     }
 }
